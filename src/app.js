4e0ed5f692713159f8dc6b89efc276ef990b616c
myriam mansouri <myriam.mansouri@gmail.com>
2019-05-01 21:01:51 -0400

    favicon

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const router = require('./routes')
 const port = process.env.PORT || 3000
 
 const app = express();
-app.use(favicon(path.join(__dirname, '..','public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, '..','public', 'favicon.png')))
+
 app.use(express.static('public'));
 
 
